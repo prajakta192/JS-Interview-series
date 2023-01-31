f058693850159f70686240a1febf9fb1eb4716a3
Prajakta Bujare <73356971+prajakta192@users.noreply.github.com>
2023-01-31 18:10:35 +0000

    primitive data type

---
+++ 000_01_primitive_data_type/index.js
@@ -0,0 +1,12 @@
+// JS DATA TYPE
+//Primitive type
+
+var dataType;     //1 => undefined
+dataType = 'Bob' // 2 => string
+dataType = 123  //  3 => number
+dataType = true //  4 => boolean
+dataType = null //  5 => null
+dataType = Symbol('hi') // symbol
+dataType = BigInt(75757575757575757575757575756) // BigInt
+
+console.log(dataType)
