52e42dbee48ae7103a0820219b7fc91003b8e239
Prajakta Bujare <73356971+prajakta192@users.noreply.github.com>
2023-02-11 16:46:59 +0530

    added typeof for data types

--- 000_01_primitive_data_type/index.js
+++ 000_01_primitive_data_type/index.js
@@ -1,12 +1,12 @@
 // JS DATA TYPE
 //Primitive type
 
-var dataType;     //1 => undefined
-dataType = 'Bob' // 2 => string
-dataType = 123  //  3 => number
-dataType = true //  4 => boolean
-dataType = null //  5 => null
-dataType = Symbol('hi') // symbol
-dataType = BigInt(75757575757575757575757575756) // BigInt
+var dataType;     //1 => undefined  typeof undefined
+dataType = 'Bob' // 2 => string     typeof string
+dataType = 123  //  3 => number     typeof number
+dataType = true //  4 => boolean    typeof boolean
+dataType = null //  5 => null       typeof object
+dataType = Symbol('hi') // symbol  unique value that can't be changed  typeof symbol
+dataType = BigInt(75757575757575757575757575756) // BigInt  // typeof BigInt
 
 console.log(dataType)
